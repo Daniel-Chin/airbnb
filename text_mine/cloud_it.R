2fceda86b75da5b8a8e405e8bfb16166d6cda2d1
Daniel Chin <daniel_chin@yahoo.com>
2020-04-29 20:57:33 -0400

    fail join with python

--- text_mine/cloud_it.R
+++ text_mine/cloud_it.R
@@ -28,7 +28,7 @@ wordcloud(popular.terms$Terms,popular.terms$n,colors=brewer.pal(8,"Dark2"),scale
 library(wordcloud)
 par(mar=c(0,0,0,0))
 load('ok.terms.interaction.rda')
-popular.terms <- filter(ok.terms,n > 3000)
+popular.terms <- filter(ok.terms,n > 1000)
 lengths(popular.terms)[1]
 wordcloud(popular.terms$Terms,popular.terms$n,colors=brewer.pal(8,"Dark2"),scale=c(
   3,.1
@@ -37,7 +37,7 @@ wordcloud(popular.terms$Terms,popular.terms$n,colors=brewer.pal(8,"Dark2"),scale
 library(wordcloud)
 par(mar=c(0,0,0,0))
 load('ok.terms.host_about.rda')
-popular.terms <- filter(ok.terms,n > 3000)
+popular.terms <- filter(ok.terms,n > 2000)
 lengths(popular.terms)[1]
 wordcloud(popular.terms$Terms,popular.terms$n,colors=brewer.pal(8,"Dark2"),scale=c(
   3,.1
@@ -46,7 +46,7 @@ wordcloud(popular.terms$Terms,popular.terms$n,colors=brewer.pal(8,"Dark2"),scale
 library(wordcloud)
 par(mar=c(0,0,0,0))
 load('ok.terms.transit.rda')
-popular.terms <- filter(ok.terms,n > 3000)
+popular.terms <- filter(ok.terms,n > 2000)
 lengths(popular.terms)[1]
 wordcloud(popular.terms$Terms,popular.terms$n,colors=brewer.pal(8,"Dark2"),scale=c(
   3,.1
@@ -55,7 +55,7 @@ wordcloud(popular.terms$Terms,popular.terms$n,colors=brewer.pal(8,"Dark2"),scale
 library(wordcloud)
 par(mar=c(0,0,0,0))
 load('ok.terms.house_rules.rda')
-popular.terms <- filter(ok.terms,n > 3000)
+popular.terms <- filter(ok.terms,n > 2400)
 lengths(popular.terms)[1]
 wordcloud(popular.terms$Terms,popular.terms$n,colors=brewer.pal(8,"Dark2"),scale=c(
   3,.1
